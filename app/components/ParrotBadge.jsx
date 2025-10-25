741f155c1a0baaef24b4ea5d896369902eabc008
krestwastaken <martynskelvin@gmail.com>
2025-10-25 12:24:40 +0100

    png

--- app/components/ParrotBadge.jsx
+++ app/components/ParrotBadge.jsx
@@ -1,25 +1,32 @@
-// components/ParrotBadge.jsx
-import React from "react";
-import "./ParrotBadge.css"; // we'll style it here
+"use client";
 
-const ParrotBadge = () => {
+import React from "react";
+import Image from "next/image";
+
+const Logo = () => {
   return (
-    <div className="container">
-      <div className="circle">
-        <svg
-          className="parrot"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M12 2C8.13 2 5 5.13 5 9v3.29c0 .69-.28 1.35-.73 1.84l-1.47 1.53c-.29.3-.29.77 0 1.06l1.47 1.53c.45.49.73 1.15.73 1.84V21c0 .55.45 1 1 1h8c3.87 0 7-3.13 7-7V9c0-3.87-3.13-7-7-7h-2z" />
-        </svg>
-        <div className="badge">B</div>
+    <div className="relative flex items-center justify-center w-64 h-64 bg-[#4AF0B5] rounded-full overflow-hidden">
+      {/* Outer Rings */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute w-[110%] h-[110%] rounded-full border-[16px] border-[#3D4E91]/60 border-t-transparent" />
+        <div className="absolute w-[130%] h-[130%] rounded-full border-[8px] border-[#4A5BCF]/70 border-b-transparent" />
       </div>
-      <div className="arc outer"></div>
-      <div className="arc inner"></div>
+
+      {/* Bird Logo */}
+      <Image
+        src="/bird-logo.png"
+        alt="Bird logo"
+        width={150}
+        height={150}
+        className="z-10"
+      />
+
+      {/* Label Circle (small “B” dot) */}
+      <div className="absolute right-[30%] top-[50%] translate-y-[-20%] bg-[#1E2A78] text-white w-6 h-6 rounded-full flex items-center justify-center text-xs font-semibold shadow-md">
+        B
+      </div>
     </div>
   );
 };
 
-export default ParrotBadge;
+export default Logo;
