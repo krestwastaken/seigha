1b39013b29375b72e931279b9ad23b2b900926d1
krestwastaken <martynskelvin@gmail.com>
2025-10-25 12:56:27 +0100

    resize

--- app/components/ParrotBadge.jsx
+++ app/components/ParrotBadge.jsx
@@ -1,15 +1,35 @@
-"use client";
-
-import React from "react";
 import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="relative flex items-center justify-center w-64 h-64 bg-[#4AF0B5] rounded-full overflow-hidden">
-      {/* Outer Rings */}
+    <div className="relative flex items-center justify-center w-44 h-44 bg-[#4AF0B5] rounded-full overflow-visible">
+      {/* Outer Arcs */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="absolute w-[110%] h-[110%] rounded-full border-[16px] border-[#3D4E91]/60 border-t-transparent" />
-        <div className="absolute w-[130%] h-[130%] rounded-full border-[8px] border-[#4A5BCF]/70 border-b-transparent" />
+        {/* Inner Arc */}
+        <div
+          className="
+            absolute w-[125%] h-[125%] 
+            border-28 border-[#3D4E91]/80 
+            rounded-full 
+            border-t-transparent
+            border-l-transparent
+            border-r-transparent
+            rotate-[-15deg]
+          "
+        />
+
+        {/* Outer Arc */}
+        <div
+          className="
+            absolute w-[150%] h-[150%] 
+            border-20 border-[#4A5BCF]/80 
+            rounded-full 
+            border-b-transparent
+            border-l-transparent
+            border-r-transparent
+            rotate-15
+          "
+        />
       </div>
 
       {/* Bird Logo */}
@@ -21,8 +41,8 @@ const Logo = () => {
         className="z-10"
       />
 
-      {/* Label Circle (small “B” dot) */}
-      <div className="absolute right-[30%] top-[50%] translate-y-[-20%] bg-[#1E2A78] text-white w-6 h-6 rounded-full flex items-center justify-center text-xs font-semibold shadow-md">
+      {/* Label Circle (“B”) */}
+      <div className="absolute right-[27%] top-[50%] translate-y-[-15%] bg-[#1E2A78] text-white w-6 h-6 rounded-full flex items-center justify-center text-xs font-semibold shadow-md">
         B
       </div>
     </div>
